2e202ca0d52c81bbbe42fb1c47510d05429cdfc8
spock166 <21161498+spock166@users.noreply.github.com>
2024-04-07 19:04:03 -0500

    Add abilit to host/join session

---
+++ Scripts/Infrastructure/Multiplayer/MultiplayerController.cs
@@ -0,0 +1,88 @@
+using Godot;
+using System;
+using System.Data.Common;
+using System.Diagnostics;
+
+public partial class MultiplayerController : Control
+{
+	[Export]
+	private int port = 8910;
+	[Export]
+	private string address = "127.0.0.1";
+
+	private ENetMultiplayerPeer peer;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Multiplayer.PeerConnected += PeerConnected;
+		Multiplayer.PeerDisconnected += PeerDisconnected;
+		Multiplayer.ConnectedToServer += ConnectedToServer;
+		Multiplayer.ConnectionFailed += ConnectionFailed;
+	}
+
+	/// <summary>
+	/// Runs when a player connects and runs on all peers
+	/// </summary>
+	/// <param name="id">ID of the player that connected.</param>
+	private void PeerConnected(long id)
+	{
+		GD.Print("Player Connected: " + id.ToString());
+	}
+
+	/// <summary>
+	/// Runs when a player disconnects and runs on all peers
+	/// </summary>
+	/// <param name="id">ID of the player that disconnected.</param>
+	private void PeerDisconnected(long id)
+	{
+		GD.Print("Player Disconnected: " + id.ToString());
+	}
+
+	/// <summary>
+	/// Runs when conenction fails and only on client.
+	/// </summary>
+	private void ConnectionFailed()
+	{
+		GD.Print("Oh No!  Connection Failed Senpai!");
+	}
+
+	/// <summary>
+	/// Runs when the conneciton is successful and only runs on the client
+	/// </summary>
+	private void ConnectedToServer()
+	{
+		GD.Print("Connected to Server Senpai!  Have Fun!");
+	}
+
+	public void OnHostButtonDown()
+	{
+		peer = new ENetMultiplayerPeer();
+		Error error = peer.CreateServer(port, maxClients: 8);
+		if(error != Error.Ok){
+			GD.Print("Error Senpai Cannot Host: " + error.ToString());
+			return;
+		}
+		peer.Host.Compress(ENetConnection.CompressionMode.RangeCoder);
+
+		Multiplayer.MultiplayerPeer = peer;
+		GD.Print("Waiting For Players...");
+	}
+
+
+	public void OnJoinButtonDown()
+	{
+		peer = new ENetMultiplayerPeer();
+		peer.CreateClient(address, port);
+
+		peer.Host.Compress(ENetConnection.CompressionMode.RangeCoder);
+		Multiplayer.MultiplayerPeer = peer;
+		GD.Print("Joining Game...");
+	}
+
+
+	public void OnStartButtonDown()
+	{
+		// Replace with function body.
+	}
+}
--- Scripts/Objects/Player.cs
+++ Scripts/Objects/Player.cs
@@ -6,41 +6,48 @@ public partial class Player : Area2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-	ScreenSize = GetViewportRect().Size;
+		ScreenSize = GetViewportRect().Size;
 	}
 
 	[Export]
-		public int Speed { get; set; } = 100; // How fast the player will move (pixels/sec).
+	public int Speed { get; set; } = 100; // How fast the player will move (pixels/sec).
 
-		public Vector2 ScreenSize; // Size of the game window.
+	public Vector2 ScreenSize; // Size of the game window.
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		var velocity = Vector2.Zero;
 
-		if(Input.IsActionPressed("move_right")){
+		if (Input.IsActionPressed("move_right"))
+		{
 			velocity += new Vector2(1, 0);
 		}
 
-		if(Input.IsActionPressed("move_left")){
+		if (Input.IsActionPressed("move_left"))
+		{
 			velocity += new Vector2(-1, 0);
 		}
 
-		if(Input.IsActionPressed("move_up")){
+		if (Input.IsActionPressed("move_up"))
+		{
 			velocity += new Vector2(0, -1);
 		}
 
-		if(Input.IsActionPressed("move_down")){
+		if (Input.IsActionPressed("move_down"))
+		{
 			velocity += new Vector2(0, 1);
 		}
 
 		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 
-		if(velocity.Length() > 0){
+		if (velocity.Length() > 0)
+		{
 			velocity = velocity.Normalized() * Speed;
 			animatedSprite2D.Play();
-		}else{
+		}
+		else
+		{
 			animatedSprite2D.Stop();
 		}
 
